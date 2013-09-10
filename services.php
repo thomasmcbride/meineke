275338bbe139402112441d9b62d1508baf5306d0
Travis Wood <travis@arrowheadadv.com>
2013-09-10 11:45:58 -0700

    services page merge

---
+++ services.php
@@ -0,0 +1,87 @@
+
+<!DOCTYPE html>
+<!--[if IE 7]><html class="ie7" lang="en"><![endif]-->
+<!--[if IE 9]><html class="ie9" lang="en"><![endif]-->
+<!--[if lte IE 8]><html class="ie8" lang="en"><![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html class="not-ie" lang="en"><!--<![endif]-->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<link rel="stylesheet" href="css/styles.css" type="text/css"/>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->   
+</head>
+
+<body>
+<?php include('includes/header.php') ?>
+
+<div class="internal-splash clearfix">
+	<div class="services clearfix wrap">
+		<p class="lato size-42"><i>my <strong>meineke</strong> gets me... <strong class="yellow">back to my life.</strong></i></p>
+
+			<select name="" class="right"><option>I Need To Change My Oil</option></select>
+
+		<aside class="">
+			<h2>Schedule An Appointment</h2>
+			<p>Fill out the form to schedule an appointment to come in. You will receive an email after we've confirmed the time and date.</p>
+			<form action="">
+				<input type="text">
+				<input type="text">
+				<input type="text">
+				<select name="" id=""></select>
+				<submit></submit>
+			</form>
+			<a href=""><img src="" alt=""></a>
+			<p>*To Receive special offers only our Facebook memebers have access to.</p>
+		</aside>
+
+		<section class="">
+			<h2>View Our Range Of Auto Services Below</h2>
+			<p>We offer a wide variety of auto services for most vehicles. If you don't see it below then call and ask us about the service you need and we'll be sure to give you our best recommended service and pricing.</p>
+			<h3>Complete Foreign &amp; Domestic Diagnostic Services</h3>
+			<ul>
+				<li>Lube, Oil &amp; Filter Changes</li>
+				<li>Power Steering Flush</li>
+				<li>Engine Flush</li>
+				<li>Air Filter</li>
+				<li>Fuel Filter</li>
+				<li>PCV Value</li>
+				<li>Fuel Injection Service</li>
+				<li>Diagnostic Services</li>
+				<li>CV Boots and Axles</li>
+				<li>Timing Belts</li>
+				<li>Battery Replacement</li>
+				<li>Starter and Alternators</li>
+				<li>Exhaust Systems</li>
+				<li>Mufflers</li>
+				<li>Tires and custom wheels</li>
+				<li>Transmission Service</li>
+				<li>Cabin Filters</li>
+				<li>Radiator Drain &amp; Fill</li>
+				<li>Differential Service</li>
+				<li>Tire Rotation</li>
+				<li>Air Conditioning Service</li>
+				<li>Brakes</li>
+				<li>Electronic Tune-Up</li>
+				<li>30,60,90 Maintenance</li>
+				<li>Water Pumps</li>
+				<li>Belts and Hoses</li>
+				<li>Shocks and Struts</li>
+				<li>Emmissions</li>
+				<li>Head light restoration</li>
+				<li>Auto &amp; truck pre purchase inspections</li>
+				<li>and much more...</li>
+			</ul>
+		</section>
+	</div>
+</div>
+
+<?php include('includes/bottom-content.php') ?>
+<?php include('includes/footer.php') ?>
+
+</body>
+
+</html>
