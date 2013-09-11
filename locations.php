bb82be2e621ee196a726f16dc35dcf2b6b7652e9
Travis Wood <travis@arrowheadadv.com>
2013-09-10 17:44:25 -0700

    creates locations page

---
+++ locations.php
@@ -0,0 +1,64 @@
+
+<!DOCTYPE html>
+<!--[if IE 7]><html class="ie7" lang="en"><![endif]-->
+<!--[if IE 9]><html class="ie9" lang="en"><![endif]-->
+<!--[if lte IE 8]><html class="ie8" lang="en"><![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html class="not-ie" lang="en"><!--<![endif]-->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<link rel="stylesheet" href="css/styles.css" type="text/css"/>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->   
+</head>
+
+<body>
+<?php include('includes/header.php') ?>
+
+<div class="internal-splash clearfix">
+	<div class="services clearfix wrap">
+		<p class="lato size-42"><i>my <span class="bold">meineke</span> gets me... <span class="bold yellow">back to my life.</span></i></p>
+
+			<select name="" class="right oil-change"><option>I Need To Change My Oil</option></select>
+
+		<aside class="locations left">
+			<h2>Our Meineke Locations</h2>
+			<p>Select an option below to make it your default location or enter your zip code.</p>
+			<input type="text" placeholder="enter zip and hit enter to submit"></input>
+			<ul>
+				<li><span>Meineke Car Care Center of Glendale</span>
+				<p>7105 N 51st Ave. Glendale, AZ <br />(480)446-8776</p>
+				<a href="">meineke.com</a></li>
+				<li><span>Meineke Car Care Center of Glendale</span>
+				<p>7105 N 51st Ave. Glendale, AZ <br />(480)446-8776</p>
+				<a href="">meineke.com</a></li>
+				<li><span>Meineke Car Care Center of Glendale</span>
+				<p>7105 N 51st Ave. Glendale, AZ <br />(480)446-8776</p>
+				<a href="">meineke.com</a></li>
+				<li><span>Meineke Car Care Center of Glendale</span>
+				<p>7105 N 51st Ave. Glendale, AZ <br />(480)446-8776</p>
+				<a href="">meineke.com</a></li>
+				<li><span>Meineke Car Care Center of Glendale</span>
+				<p>7105 N 51st Ave. Glendale, AZ <br />(480)446-8776</p>
+				<a href="">meineke.com</a></li>
+			</ul>
+
+			<a href=""><img src="img/facebook.png" alt="" class="fb"></a>
+			<p>*To Receive special offers only our Facebook memebers have access to.</p>
+		</aside>
+
+		<section class="location-content right">
+			<img src="img/map-ph.jpg">
+		</section>
+	</div>
+</div>
+
+<?php include('includes/bottom-content.php') ?>
+<?php include('includes/footer.php') ?>
+
+</body>
+
+</html>
