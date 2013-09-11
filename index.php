bc9d52c7c707cfcdedb71a6d6ab0ef08490b4368
Eddie SanMarco <eddie@arrowheadadv.com>
2013-09-10 18:01:57 -0700

    this is the homepage stuff

--- index.php
+++ index.php
@@ -18,8 +18,67 @@
 <body>
 <?php include('includes/header.php') ?>
 
-
-
+<div class="splash home">
+		<div class="wrap clearfix home-splash">
+			<h1>my meineke gets me...<br /> back to my life.</h1>
+		</div>
+</div>
+<div class="clearfix wrap">
+<aside class="left scheduler">
+	<div class="schedule">
+		<h2 class="center">Schedule An <span class="yellow size-30">Appointment</span></h2>
+		<p class="center">Fill out the form to schedule an appointment to come in. You will receive an email after we've confirmed the time and date.</p>
+		<form action="">
+			<input type="text" placeholder="Full Name">
+			<input type="text" placeholder="Email Address">
+			<input type="text" placeholder="Phone Number">
+			<select name="" id="">
+				<option value="">Best Time to reach you</option>
+			</select>
+			<submit class="submit-form"><a href="">SUBMIT FORM</a></submit>
+		</form>
+	</div>
+	<a href=""><img src="img/facebook.png" alt="" class="fb"></a>
+	<p>*To Receive special offers only our Facebook memebers have access to.</p>
+</aside>
+<section class="right content seventy">
+	<div class="clearfix">
+		<h1>Limited Time Offers</h1>
+		<p>Below are our limited time offered specials. Click to either print the coupon, or for more details if you are interested in any of the service specials. You can also call us if you have any additional questions regarding the savings.</p>
+		<div class="clearfix">
+			<div class="fourty9 left box">
+				<h1>Oil Change & Filter</h1>
+				<p class="center"><img src="img/box-1.png" alt=""></p>
+				<p class="offer-copywrite">OFFER ENDS SEPTEMBER 1, 2013. Offer good on most vehicles. Click to see details</p>
+				<p class="center"><a href="#">Print Coupon</a> <a href="#">More Details</a></p>
+				<p class="center"><img src="img/filters.png" alt="Filter"></p>
+			</div>
+			<div class="fourty9 right box">
+				<h1>Car care check-up</h1>
+				<p class="center"><img src="img/box-2.png" alt=""></p>
+				<p class="offer-copywrite">OFFER ENDS SEPTEMBER 1, 2013. Offer good on most vehicles. Click to see details</p>
+				<p class="center"><a href="#">Print Coupon</a> <a href="#">More Details</a></p>
+				<p class="center"><img src="img/socket.png" alt="Socket"></p>
+			</div>
+			<div class="clearfix"></div>
+			<div class="fourty9 left box">
+				<h1>Interstate Batteries</h1>
+				<p class="center"><img src="img/box-3.png" alt=""></p>
+				<p class="offer-copywrite">OFFER ENDS SEPTEMBER 1, 2013. Offer good on most vehicles. Click to see details</p>
+				<p class="center"><a href="#">Print Coupon</a> <a href="#">More Details</a></p>
+				<p class="center"><img src="img/battery.png" alt="Battery"></p>
+			</div>
+			<div class="fourty9 right box">
+				<h1>brake pads & shoes</h1>
+				<p class="center"><img src="img/box-4.png" alt=""></p>
+				<p class="offer-copywrite">OFFER ENDS SEPTEMBER 1, 2013. Offer good on most vehicles. Click to see details</p>
+				<p class="center"><a href="#">Print Coupon</a> <a href="#">More Details</a></p>
+				<p class="center"><img src="img/brakes.png" alt="Brakes"></p>
+			</div>
+		</div>
+	</div>
+</section>
+</div>
 <?php include('includes/bottom-content.php') ?>
 <?php include('includes/footer.php') ?>
 
